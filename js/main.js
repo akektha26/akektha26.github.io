4011fe834c2b82e91b47773aa73106c3bddf6ef2
akektha26 <akektha26@naver.com>
2020-10-30 12:07:12 +0900

    커밋

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@
 	aname('김일국'); //함수호출
 	*/
 	$(document).ready(function(){
+		// 상단으로 바로가기 버튼 클릭 위에 스무스스크롤이라는 자바라이브러리를 가져와 썼기 때문에 가능.
+	$(".to_top a").smoothScroll();
 		//alert('콜백함수 실행');
 		$(".openMOgnb").click(function(){
 			//alert();//디버그용
